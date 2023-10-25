446a625bac22dd08433f1176ea2b096ff56db444
Sneaken <924393527@qq.com>
2023-10-25 10:53:23 +0800

    chore: 不存在中文的时候或者异常的时候来一个提示

--- detect_chinese_characters.js
+++ detect_chinese_characters.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 // 递归检测指定目录下的文件
+let count = 0;
 function scanDirectoryForChineseCharacters(directoryPath) {
   const files = fs.readdirSync(directoryPath);
   files.forEach((file) => {
@@ -10,6 +11,7 @@ function scanDirectoryForChineseCharacters(directoryPath) {
     if (fs.statSync(filePath).isDirectory()) {
       // 如果是子目录，递归检测子目录
       if (containsChineseCharacters(filePath)) {
+        count++;
         console.log(`目录包含中文字符: ${filePath}`);
       }
       scanDirectoryForChineseCharacters(filePath);
@@ -17,6 +19,7 @@ function scanDirectoryForChineseCharacters(directoryPath) {
       // 如果是文件，检查文件名是否包含中文字符
       if (containsChineseCharacters(filePath)) {
         console.log(`文件包含中文字符: ${filePath}`);
+        count++;
       }
     }
   });
@@ -28,5 +31,12 @@ function containsChineseCharacters(text) {
   return chineseRegex.test(text);
 }
 
-const targetDirectory = process.argv[2] || '.'; // 替换为你要检测的目录路径 默认当前文件夹
-scanDirectoryForChineseCharacters(targetDirectory);
+try {
+  const targetDirectory = process.argv[2] || '.'; // 替换为你要检测的目录路径 默认当前文件夹
+  scanDirectoryForChineseCharacters(targetDirectory);
+  if (count === 0) {
+    console.log("不存在中文")
+  }
+} catch (e) {
+  console.error("异常", e)
+}
